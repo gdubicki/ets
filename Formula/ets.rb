900f0279c7ea8bb7f1c1626c95680c49b640fec3
Greg Dubicki <grzegorz.dubicki@gmail.com>
2024-04-30 08:55:49 +0100

    Fix v0.2.2 release to homebrew

--- Formula/ets.rb
+++ Formula/ets.rb
@@ -2,7 +2,7 @@ class Ets < Formula
   desc "Command output timestamper"
   homepage "https://github.com/gdubicki/ets"
   url "https://github.com/gdubicki/ets/archive/v0.2.2.tar.gz"
-  sha256 "7d7bf592cb36da25c941a10989622a0a0dd1a99c5dc037e840a25462a5401d66"
+  sha256 "decfe53acb1a912b51c81c22695f92caa72c79f1d6ec5ef23d0fb6be4e1f2db5"
 
   depends_on "go" => :build
 
