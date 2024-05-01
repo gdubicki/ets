1f8f52156598600fde04d13389b3f897d261b440
Greg Dubicki <grzegorz.dubicki@gmail.com>
2024-05-01 12:33:27 +0100

    Make it work Homebrew better

--- tools/update-readme.sh
+++ tools/update-readme.sh
@@ -6,6 +6,13 @@
 
 setopt errexit
 
+# On MacOS you may have GNU sed as 'gsed' - if so, use it
+if type gsed &> /dev/null; then
+  sed_program="gsed"
+else
+  sed_program="sed"
+fi
+
 here=$0:A:h
 root=$here:h
 
@@ -14,7 +21,7 @@ echo '\n```' >>$tmpfile
 man $root/ets.1 | sed -r 's/.\x08//g' >>$tmpfile
 echo '```\n' >>$tmpfile
 
-sed -i "/<!-- begin manpage -->/,/<!-- end manpage -->/ {
+$sed_program -i "/<!-- begin manpage -->/,/<!-- end manpage -->/ {
   /<!-- begin manpage -->/ {
     r $tmpfile
     n
